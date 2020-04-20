ed37f8a3860ae2976fdbebfe5cc6bf3690435303
Mario Garza <mario.garza02@utrgv.edu>
2020-04-20 04:47:42 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -32,7 +32,9 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-  
+  shipping_manifest["muskets"]=2
+  shipping_manifest["gun powder"]=4
+  shipping_manifest
 
 
 
